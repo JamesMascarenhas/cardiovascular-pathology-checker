ca4cb255b1cba58ae4e7735b70b68370b1603ad6
James Mascarenhas <james.mas27@gmail.com>
2024-12-02 00:56:55 -0500

    Create tables.py

---
+++ project_directory/database_method/tables.py
@@ -0,0 +1,37 @@
+# Pathologies table
+pathologies_table = [
+    "Atherosclerosis", "Ischemic Heart Disease", "Acute Coronary Syndromes", "Valvular Heart Disease", 
+    "Heart Failure", "The Cardiomyopathies", "Cardiac Arrhythmias", "Pericardium Diseases", 
+    "Peripheral Vasculature Diseases", "Congenital Heart Disease"
+]
+
+# Symptoms table
+symptoms_table = [
+    "Anxiety", "Blue or gray skin", "Chest pain", "Coldness in the lower leg or foot",
+    "Consistent cough", "Cough with white or pink mucus and blood", "Difficulty concentrating",
+    "Drooping face muscles", "Erectile dysfunction", "Fainting", "Fast heartbeat",
+    "Fatigue", "Hair loss", "Heart palpitations", "Indigestion", "Irregular heartbeat",
+    "Leg numbness or weakness", "Leg pain when walking", "Lighthead or dizzy",
+    "Lightheaded or dizzy", "Muscle pain or cramping", "Nausea and vomiting",
+    "Neck or jaw pain", "No or weak pulse in the leg or foot", "Numbness or weakness in the arms or legs",
+    "Pain spreading to shoulders and neck", "Pain using arms", "Pressure or heaviness on the chest",
+    "Radiating pain from chest to extremities", "Rapid weight gain", "Reduced ability to exercise",
+    "Shiny skin on the legs", "Shoulder or arm pain", "Skin color changes on the legs",
+    "Slow heartbeat", "Slow-growing toenails", "Slurred speech", "Sores on the toes, feet or legs",
+    "Sudden or temporary loss of vision in one eye", "Swelling", "Swelling of the ankles and feet",
+    "Swelling of the belly area", "Sweating", "Trouble speaking", "Unusual fatigue", "Wheezing"
+]
+# Risk Factors Table
+risk_factors_table = [
+    "Alcohol use", "Blood infections", "Cancer", "Chemotherapy",
+    "Cocaine or amphetamine use", "COVID-19 infection", "Diabetes",
+    "Early menopause", "Electrolyte imbalance", "Heart disease",
+    "Heart problems present at birth", "History of Atherosclerosis",
+    "History of congenital heart disease", "History of hypertension",
+    "History of Peripheral Vasculature Diseases", "History of preeclampsia",
+    "Hypertension", "Hyperlipidemia", "Inflammatory Bowel Disease", "Injury to the heart or chest",
+    "Kidney failure", "Lack of Physical Activity", "Lupus", "Metabolic Disorders",
+    "Obesity", "Old age", "Pregnancy complications", "Previous heart attack",
+    "Psoriasis", "Radiation exposure", "Rheumatic fever", "Rubella",
+    "Sleep apnea", "Thyroid disease", "Tobacco Smoking", "Viral infections"
+]
